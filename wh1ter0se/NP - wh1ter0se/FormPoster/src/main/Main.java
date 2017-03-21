80008b46b33bb4f2f8dc8da097525e026e178299
John Doe <ipad3000@yahoo.com>
2017-03-21 09:02:40 -0500

    Worked on stuff.

--- wh1ter0se/NP - wh1ter0se/FormPoster/src/main/Main.java
+++ wh1ter0se/NP - wh1ter0se/FormPoster/src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-
+		Scanner in = new Scanner(System.in);
+		while (in.next().toLowerCase().equals("")) {
+			
+		}
 	}
 
 }
