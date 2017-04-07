c2d01b60f677afba83e5860aba514de026ab663d
1914024 <1914024@YHSC128C08.y115.org>
2017-04-07 09:38:23 -0500

    Worked.

--- wh1ter0se/NP - wh1ter0se/FormPoster/src/output/Output.java
+++ wh1ter0se/NP - wh1ter0se/FormPoster/src/output/Output.java
@@ -5,9 +5,10 @@ import java.net.*;
 
 public class Output {
 	private static String NAME;
+	private static String OUTPUT;
 	private static URL URL;
 	
-	public static void sendToServer(String n) {
+	public static String sendToServer(String n) {
 		try {
 			NAME = n;
 			URL = new URL("http://colton.yhscs.us/?name=" + NAME);
@@ -17,10 +18,11 @@ public class Output {
 			Reader reader = new InputStreamReader(buffer);
 
 			int c;
-			while ((c = reader.read()) != -1) { System.out.print((char)c); }
-			
+			while ((c = reader.read()) != -1) { OUTPUT += (char) c; }
+			return OUTPUT;
+			// just use echo in the php file, and figure out where the code for the name game went
 		} catch (MalformedURLException e) { System.err.println("Output:sendToServer; MalformedURLException"); } 
 		  catch (IOException e) 		  { System.err.println("Output:sendToServer; IOException"); }
-		
+		return "ERROR";
 	}
 }
