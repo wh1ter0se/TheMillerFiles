76087997deee673d707c19d4da6646fdadca85af
wh1ter0se <ipad3000@yahoo.com>
2017-05-09 17:55:14 -0500

    Added Sigar.

--- wh1ter0se/NP - wh1ter0se/LatinDictionary/src/main/Main.java
+++ wh1ter0se/NP - wh1ter0se/LatinDictionary/src/main/Main.java
@@ -12,12 +12,16 @@ public class Main {
 		System.out.println("Enter the word in English.");
 		Scanner in = new Scanner(System.in);
 		ENGLISH_WORD = in.nextLine();
-		int i = 0; String print = ENGLISH_WORD;
-		while (true) {
-			i++;
-			System.out.println(ServerContact.getLatin(ENGLISH_WORD) + "\r\n");
-			print = print.substring(print.indexOf(" ", print.indexOf(" ") + i));
-			if (print == "-1") { break; }}
+		if (ENGLISH_WORD.contains(" ")) {
+			int i = 0; String print = ENGLISH_WORD;
+			while (true) {
+				i++;
+				System.out.println(ServerContact.getLatin(ENGLISH_WORD) + "\r\n");
+				print = print.substring(print.indexOf(" ", print.indexOf(" ") + i));
+				if (print.indexOf(" ", print.indexOf(" ") + (i + 1)) == -1) { break; }}}
+		else {
+			
+		}
 		System.out.println(ServerContact.getLatin(ENGLISH_WORD));
 	}}
 
