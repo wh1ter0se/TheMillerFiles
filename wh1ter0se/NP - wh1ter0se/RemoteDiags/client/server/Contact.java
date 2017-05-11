eb2da12d91e86e90c07e83f77d7ef97a25e75e02
1914024 <1914024@YHSC128C08.y115.org>
2017-05-11 10:15:57 -0500

    Worked.

--- wh1ter0se/NP - wh1ter0se/RemoteDiags/client/server/Contact.java
+++ wh1ter0se/NP - wh1ter0se/RemoteDiags/client/server/Contact.java
@@ -1,7 +1,18 @@
 package server;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Contact {
-	public static String[] getModules() {
+	public final static String SERVER_ADDRESS = "https://www.colton.yhscs.us/";
+	public static InetAddress SERVER_INET;
+	
+	public Contact() {
+		SERVER_INET = null;
+		try { SERVER_INET = InetAddress.getByName(SERVER_ADDRESS); } catch (UnknownHostException e) { System.err.println(e); }
+	}
+	
+	public static String[] getModules() { //TODO actually retrieve modules from the database
 		String[] modules = new String[1];
 		return modules;
 	}
